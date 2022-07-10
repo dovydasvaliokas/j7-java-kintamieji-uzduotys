cff5965f210d209ce26ab06150458a970ed76fb7
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 21:05:08 +0300

    Sukurtas antros uzduoties failas

---
+++ src/Uzduotis2.java
@@ -0,0 +1,14 @@
+/*
+Užduotis 2
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurkite papildomą funkciją, kuri gauna visas tris trikampio kraštines ir apskaičiuoja (returnina) perimetrą.
+3. Susikurkite papildomą funkciją, kuri gauna visas tris trikampio kraštines ir naudojant Herono formulę apskaičiuoja bet kokio trikampio plotą. (Herono formulei reikalingas perimetras, dėl to jo funkciją ir pasidarėte antrame punkte). Herono formulė:
+
+ */
+public class Uzduotis2 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
