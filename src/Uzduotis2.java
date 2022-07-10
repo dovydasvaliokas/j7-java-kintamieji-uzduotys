cb5e3d8b8f913f14a7f9ba866289ab0c5b060674
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 21:14:32 +0300

    Panaudoju funkcijas pagrindineje programoje - pasitikrinti ar jos veikia gerai

--- src/Uzduotis2.java
+++ src/Uzduotis2.java
@@ -3,10 +3,12 @@ Užduotis 2
 1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
 2. Susikurkite papildomą funkciją, kuri gauna visas tris trikampio kraštines ir apskaičiuoja (returnina) perimetrą.
 3. Susikurkite papildomą funkciją, kuri gauna visas tris trikampio kraštines ir naudojant Herono formulę apskaičiuoja bet kokio trikampio plotą. (Herono formulei reikalingas perimetras, dėl to jo funkciją ir pasidarėte antrame punkte). Herono formulė:
-
+   Pasitikrinti galima: https://www.calculator.net/triangle-calculator.html
  */
 public class Uzduotis2 {
     public static void main(String[] args) {
+        System.out.println("trikampioPerimetras(5, 6, 8) = " + trikampioPerimetras(5, 6, 8));
+        System.out.println("trikampioPlotas(5, 6, 8) = " + trikampioPlotas(5, 6, 8));
 
     }
 
