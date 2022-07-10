668ebc84f0ecc272dfc502e7a9384c85a4fda582
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-10 21:15:30 +0300

    Sukurtas trecios uzduoties failas

---
+++ src/Uzduotis3.java
@@ -0,0 +1,11 @@
+/*
+Užduotis 3
+1. Susikurkite Java projektą, susikurkite Programa.java failą (class), tame faile susikurkite main funkciją.
+2. Susikurti papildomą funkciją, kuri gauna trijų linijų ilgius ir grąžina true arba false priklausomai ar su tom trim linijomis galima sudaryti trikampį. Jog būtų galima sudaryti trikampį, tai nei viena kraštinė turi nebūti nei didesnė arba nei lygi kitų dviejų kraštinių sumai. T.y., a < b + c ir t.t. su visoms kraštinėmis.
+Jūsų užduočiai if nėra būtinas, kadangi galima iš karto return naudoti. Tačiau, jei norite padaryti su if, tai jis yra realiai viskas identiška kaip ir javascript
+ */
+public class Uzduotis3 {
+    public static void main(String[] args) {
+
+    }
+}
